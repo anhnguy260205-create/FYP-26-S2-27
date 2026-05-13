565200684a638f990e261d276fcb2324b5850875
Kim Anh <kimanh.work26@gmail.com>
2026-05-13 18:09:47 +0800

    changed color

--- frontend/src/pages/user/Homepage.jsx
+++ frontend/src/pages/user/Homepage.jsx
@@ -9,7 +9,7 @@ function Homepage() {
 
   return (
 
-    <div className="min-h-screen flex flex-col bg-gradient-to-br from-slate-950 via-blue-950 to-slate-900 text-white ">
+    <div className="min-h-screen flex flex-col bg-linear-to-br from-slate-950 via-blue-950 to-slate-900 text-white ">
       <Link
         className="px-4 py-2 bg-blue-500 text-white rounded mt-4 self-center"
         to="/register">
