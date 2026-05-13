93801b6fd1bb736f86aa139db34e5cc7fab6a507
Kim Anh <kimanh.work26@gmail.com>
2026-05-13 19:24:01 +0800

    changes

--- frontend/src/pages/user/Homepage.jsx
+++ frontend/src/pages/user/Homepage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import RegistrationPage from "./RegistrationPage.jsx";
 import Footer from "../../components/Footer.jsx";
 
