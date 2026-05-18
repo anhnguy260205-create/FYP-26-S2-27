f9f086c550c903dd206744f31095c037462c5c39
Kim Anh <kimanh.work26@gmail.com>
2026-05-18 13:31:11 +0800

    update the stock dashboard

--- frontend/src/pages/investor/AStockDashBoardPage.jsx
+++ frontend/src/pages/investor/AStockDashBoardPage.jsx
@@ -4,16 +4,24 @@ import { motion } from "framer-motion";
 import { useParams} from "react-router-dom"
 import useLiveStocks from "../../api/useLiveStocks.js";
 import InteractiveChart from "../../components/InteractiveChart.jsx";
-function SecondLevel(){
-    const { symbol } = useParams()
-    const selectedStock = symbol?.toUpperCase()
-    const { stocks, marketStatus, candles, lastUpdated, error } = useLiveStocks();
-    const stockCandles = candles?.[selectedStock]??[];
-    return(
-    <div style={{paddingTop: "20px"}}>
-        <InteractiveChart data={stockCandles} />
-    </div>
-   );
+function formatNumber(num){
+    if(!num) return"0";
+
+    const format = (value, suffix)=> parseFloat(value.toFixed(1)) + suffix;
+    
+    if (num >= 1_000_000_000) {
+        return format(num / 1_000_000_000, "B");
+    }
+
+    if (num >= 1_000_000) {
+        return format(num / 1_000_000, "M");
+    }
+
+    if (num >= 1_000) {
+        return format(num / 1_000, "K");
+    }
+
+    return num.toString();
 }
 function companyName(symbol) {
   const names = {
@@ -38,6 +46,71 @@ function FirstLevel(){
         </div>
     );
 }
+function SecondLevel({ stock, stockCandles }) {
+    if (!stock) return null;
+
+    const open = stock.open;
+    const high = stock.high;
+    const low = stock.low;
+    const volume = stock.volume;
+    const avgVolume =
+        stockCandles.length > 0
+            ? stockCandles.reduce(
+                  (sum, candle) => sum + candle.volume,
+                  0
+              ) / stockCandles.length
+            : 0;
+    return (
+        <div
+            className="w-209 flex justify-between border-2 bg-white p-4 rounded-md text-black"
+            style={{ marginTop: "20px" }}
+        >
+            <div className="flex flex-col items-start">
+                <p>Open</p>
+                <p>{open.toFixed(2)}</p>
+            </div>
+
+            <span className="text-gray-500">|</span>
+
+            <div className="flex flex-col items-start">
+                <p>High</p>
+                <p>{high.toFixed(2)}</p>
+            </div>
+
+            <span className="text-gray-500">|</span>
+
+            <div className="flex flex-col items-start">
+                <p>Low</p>
+                <p>{low.toFixed(2)}</p>
+            </div>
+
+            <span className="text-gray-500">|</span>
+
+            <div className="flex flex-col items-start">
+                <p>Volume</p>
+                <p>{formatNumber(volume)}</p>
+            </div>
+
+            <span className="text-gray-500">|</span>
+
+            <div className="flex flex-col items-start">
+                <p>Avg Volume</p>
+                <p>{formatNumber(avgVolume)}</p>
+            </div>
+        </div>
+    );
+}
+function ThirdLevel(){
+    const { symbol } = useParams()
+    const selectedStock = symbol?.toUpperCase()
+    const { stocks, marketStatus, candles, lastUpdated, error } = useLiveStocks();
+    const stockCandles = candles?.[selectedStock]??[];
+    return(
+    <div style={{paddingTop: "20px"}}>
+        <InteractiveChart data={stockCandles} />
+    </div>
+   );
+}
 function AStockDashBoardPage(){
     const { symbol } = useParams()
     const selectedStock = symbol?.toUpperCase()
@@ -49,7 +122,8 @@ function AStockDashBoardPage(){
             <GeneralHeader/>
             <main className="flex-1 p-7.5">
                 <FirstLevel/>
-                <SecondLevel/>
+                <SecondLevel stock={stocks[selectedStock]} stockCandles={stockCandles}/>
+                <ThirdLevel/>
             </main>
             <Footer/>
         </motion.div>
