f1a37938477ec001318450ce69a1a825f072aaa6
Kim Anh <kimanh.work26@gmail.com>
2026-05-21 23:26:51 +0800

    login

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,31 +1,21 @@
+// src/api/userapi.js
+
+const BASE_URL = "http://127.0.0.1:8000/user";
+
 export const createAccount = async (formData) => {
-
-  const response = await fetch(
-    "http://127.0.0.1:8000/user/create-account",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    }
-  );
-
+  const response = await fetch(`${BASE_URL}/create-account`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formData),
+  });
   return await response.json();
 };
 
 export const loginAccount = async (formData) => {
-
-  const response = await fetch(
-    "http://127.0.0.1:8000/user/login",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    }
-  );
-
+  const response = await fetch(`${BASE_URL}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formData),
+  });
   return await response.json();
 };
