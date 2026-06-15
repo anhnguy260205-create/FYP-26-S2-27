0f73c7782e3293e95f1212a9b53b36304205fa06
Kim Anh <kimanh.work26@gmail.com>
2026-06-15 17:00:38 +0800

    fix

--- backend/app/entity/models/investor.py
+++ backend/app/entity/models/investor.py
@@ -126,5 +126,5 @@ def seed_investor_account():
         password="password",
         phone_number=12343243,
         address="123 Kim Street",
-        stock_level="Beginner"
+        stock_level="Basic"
     )
