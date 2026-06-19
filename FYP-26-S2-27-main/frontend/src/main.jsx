604a70f9d2b4efd4281dfb55b3f8b576ad76390e
Lan <100503407+daggerzzz@users.noreply.github.com>
2026-06-19 14:51:29 +0800

    Revert "Forum"

--- FYP-26-S2-27-main/frontend/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./style/index.css";
-//starts React and displays your app in the browser.
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
-);
