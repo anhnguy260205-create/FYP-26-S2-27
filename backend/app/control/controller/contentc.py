09a9fc519a525d345603c4c92868a6636ce7b640
Kim Anh <kimanh.work26@gmail.com>
2026-06-22 13:11:05 +0800

    fixing name convention

---
+++ backend/app/control/controller/contentc.py
@@ -0,0 +1,16 @@
+from app.entity.models.contentmanagement import ContentManagement
+
+
+class GetAllContentController:
+    def getAll(self):
+        return ContentManagement.get_all()
+
+
+class GetContentBySectionController:
+    def getBySection(self, section: str):
+        return ContentManagement.get_by_section(section)
+
+
+class UpdateContentController:
+    def update(self, content_id: str, title: str, description: str):
+        return ContentManagement.update(content_id, title, description)
